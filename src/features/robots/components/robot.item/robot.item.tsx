cf279c75b16443ace63cfebf059df17857e01165
marcos schernetzki <marcoschernetzki@gmail.com>
2022-10-27 20:17:09 +0200

    add robots and css

--- src/features/robots/components/robot.item/robot.item.tsx
+++ src/features/robots/components/robot.item/robot.item.tsx
@@ -1,5 +1,5 @@
 import { Robot } from "../../models/robot";
-import styles from "../robot.item.module.css";
+import styles from "./robot.item.module.css";
 
 export function RobotItem({ item }: { item: Robot }) {
     // const { handlerEraser, handlerComplete } = useContext(TodoContext);
@@ -14,9 +14,16 @@ export function RobotItem({ item }: { item: Robot }) {
 
     return (
         <li className={styles.host}>
-            <input type="checkbox" />
-            <span>{item.id}</span> -<span>{item.name}</span>
-            <span className="button">🗑️</span>
+            <img src={item.img} alt={item.name} />
+            <div>
+                <span>
+                    <h2>Nombre: {item.name}</h2>
+                </span>
+                <span>Velocidad: {item.speed}</span>
+                <span>Resistencia: {item.endurance}</span>
+                <span>Fecha de nacimiento: {item.date}</span>
+                <span className="button">🗑️</span>
+            </div>
         </li>
     );
 }
--- src/infrastructure/components/layout/layout.tsx
+++ src/infrastructure/components/layout/layout.tsx
@@ -1,11 +1,12 @@
-import { Footer } from '../footer/footer';
-import { Header } from '../header/header';
+import { RobotList } from "../../../features/robots/components/robot.list/robot.list";
+import { Footer } from "../footer/footer";
+import { Header } from "../header/header";
 
 export function Layout({ children }: { children: JSX.Element }) {
     return (
         <>
             <Header></Header>
-            {children}
+            <RobotList></RobotList>
             <Footer></Footer>
         </>
     );
