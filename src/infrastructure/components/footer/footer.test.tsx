055c235c068075a28530156d16804ddd74393abb
marcos schernetzki <marcoschernetzki@gmail.com>
2022-10-27 19:02:54 +0200

    add data robots

--- src/infrastructure/components/footer/footer.test.tsx
+++ src/infrastructure/components/footer/footer.test.tsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Footer } from './footer';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Footer } from "./footer";
 
-describe('Given Footer component', () => {
-    describe('When we render the component', () => {
+describe("Given Footer component", () => {
+    describe("When we render the component", () => {
         beforeEach(() => {
             // eslint-disable-next-line testing-library/no-render-in-setup
             render(<Footer />);
         });
-        test('Then it should display "ISDI"', () => {
-            const element = screen.getByText(/ISDI/i);
-            expect(element).toBeInTheDocument();
-        });
-        test('Then it should display today data', () => {
-            const expectedDate = new Date().toLocaleString();
-            const element = screen.getByText(expectedDate);
+        test('Then it should display "By Marcos"', () => {
+            const element = screen.getByText(/By Marcos/i);
             expect(element).toBeInTheDocument();
         });
     });
--- src/infrastructure/components/header/header.test.tsx
+++ src/infrastructure/components/header/header.test.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter as Router } from 'react-router-dom';
-import { Header } from './header';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
+import { Header } from "./header";
 
-describe('Given Header component', () => {
-    describe('When we render the component', () => {
-        test('Then it should display "Learning react"', () => {
+describe("Given Header component", () => {
+    describe("When we render the component", () => {
+        test('Then it should display "Robots"', () => {
             render(
                 <Router>
                     <Header />
                 </Router>
             );
-            const element = screen.getByText(/Learning React/i);
+            const element = screen.getByText(/Robots/i);
             expect(element).toBeInTheDocument();
         });
     });
