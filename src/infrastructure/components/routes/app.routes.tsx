1550cde09a2c0dfe4c4c6b85dadedadd046f2d4f
marcos schernetzki <marcoschernetzki@gmail.com>
2022-10-27 18:15:14 +0200

    add initial files

---
+++ src/infrastructure/components/routes/app.routes.tsx
@@ -0,0 +1,18 @@
+// import { Navigate, Route, Routes } from 'react-router-dom';
+// import GotPage from '../../../features/got/pages/got.page';
+// import HomePage from '../../../features/home/page/home.page';
+// import TodoPage from '../../../features/todo/pages/todo.page';
+
+import { Routes } from "react-router-dom";
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            {/* <Route path="home" element={<HomePage></HomePage>}></Route>
+            <Route path="todo" element={<TodoPage></TodoPage>}></Route>
+            <Route path="got" element={<GotPage></GotPage>}></Route>
+            <Route path="" element={<HomePage></HomePage>}></Route>
+            <Route path="*" element={<Navigate replace to="" />}></Route> */}
+        </Routes>
+    );
+}
