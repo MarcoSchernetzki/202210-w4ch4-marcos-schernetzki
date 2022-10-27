f02d5e28509c855c85483d29017d20200d764c22
marcos schernetzki <marcoschernetzki@gmail.com>
2022-10-27 21:57:29 +0200

    add delete and test

--- src/features/robots/components/robot.list/robot.list.tsx
+++ src/features/robots/components/robot.list/robot.list.tsx
@@ -1,12 +1,8 @@
 import { ROBOTS } from "../../models/data";
 import { Robot } from "../../models/robot";
 import { RobotItem } from "../robot.item/robot.item";
-import styles from "./robot.list.module.css";
 
 export function RobotList() {
-    // const title = "TaskList component";
-    // const { robots } = useContext(TodoContext);
-
     return (
         <section>
             <ul>
--- src/infrastructure/context/provider.tsx
+++ src/infrastructure/context/provider.tsx
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import { useState } from "react";
 import { Robot } from "../../features/robots/models/robot";
 import { RobotContext } from "./context";
